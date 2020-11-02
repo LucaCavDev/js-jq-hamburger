796e728880e3f0a555a33ea406b70c56b4f1be44
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-11-02 17:29:03 +0100

    inserito tra i commenti 3° metodo (metodo1+window.matchmedia)
    risolve il problema del metodo 1, il fatto che l'hamburger non si chiudeva se si allargava lo schermo a +di 1000px

--- main.js
+++ main.js
@@ -9,18 +9,26 @@
 //partendo da hamburger menu chiuso, lo devo aprire al click
 // scelgo la classe fas perchè è la più stringente ed è l'unica che posso usare sia per chiudere che aprire
 
-//METODO 1 -
+//=============================================
+// METODO 1 -
 // problema: quando ho l'hamburger menu aperto, se allargo displey oltre i 1000, mi resta attivo il menu hamburgher, sarebbe meglio che si chiuda
 
-$('.header-right .fas').click(function() {
-  $('.hamburger-menu').show();
-});
+// $('.header-right .fas').click(function() {
+//   $('.hamburger-menu').show();
+// });
+//
+// $('.hamburger-menu .fas').click(function() {
+//   $('.hamburger-menu').hide();
+// });
 
-$('.hamburger-menu .fas').click(function() {
-  $('.hamburger-menu').hide();
-});
+//-----------------------------------------------------------
 
 
+
+
+
+
+//=============================================
 // METODO 2 - lavoro su classe .hamburger-menu.active
 
 // $('.header-right .fas').click(function() {
@@ -32,5 +40,25 @@ $('.hamburger-menu .fas').click(function() {
 // });
 
 // in questo modo dato che active si attiva solo sotto i 1000 px, si chiude quando vado sopra i 1000px
+//-----------------------------------------------------------
+
+
+//=============================================
+//METODO 3
+//in questo metodo ritocco il metodo 1 eliminando il problema della non chiusura dell'hamburger quando il display va a +1000 px usando il winddow.matchMedia
+
+
+// if (window.matchMedia("(max-width: 1000px)").matches) {
+//   $('.header-right .fas').click(function() {
+//     $('.hamburger-menu').show();
+//   });
+//
+//   $('.hamburger-menu .fas').click(function() {
+//     $('.hamburger-menu').hide();
+//   });
+// };
+
+//-----------------------------------------------------------
+
 
 console.log('ciao');
