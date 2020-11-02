66e77e2969ba42bf559cc30b72b48c3f84dd4c20
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-11-02 18:06:27 +0100

    per perfezionare l'esercizio senza usare la classe che è gia nel mediaquery di css devo usare l'evento resize di jQuery.

--- main.js
+++ main.js
@@ -9,6 +9,11 @@
 //partendo da hamburger menu chiuso, lo devo aprire al click
 // scelgo la classe fas perchè è la più stringente ed è l'unica che posso usare sia per chiudere che aprire
 
+//METODO DOVE VIENE AL 100% è il 2, dove si aggiunge la classe active che è gia inserita nel mediaquery
+
+
+
+
 //=============================================
 // METODO 1 -
 // problema: quando ho l'hamburger menu aperto, se allargo displey oltre i 1000, mi resta attivo il menu hamburgher, sarebbe meglio che si chiuda
@@ -26,42 +31,36 @@
 
 
 
-
-
 //=============================================
 // METODO 2 - lavoro su classe .hamburger-menu.active
 //
-// $('.header-right .fas').click(function() {
-//   $('.hamburger-menu').addClass('active');
-// });
-//
-// $('.hamburger-menu .fas').click(function() {
-//   $('.hamburger-menu').removeClass('active');
-// });
+$('.header-right .fas').click(function() {
+  $('.hamburger-menu').addClass('active');
+});
+
+$('.hamburger-menu .fas').click(function() {
+  $('.hamburger-menu').removeClass('active');
+});
 
 // in questo modo dato che active si attiva solo sotto i 1000 px, si chiude quando vado sopra i 1000px
 //-----------------------------------------------------------
 
 
 //=============================================
-//METODO 3
+//METODO 3  --
 //in questo metodo ritocco il metodo 1 eliminando il problema della non chiusura dell'hamburger quando il display va a +1000 px usando il winddow.matchMedia
 
 
-if (window.matchMedia("(max-width: 1000px)").matches) {
-  $('.header-right .fas').click(function() {
-    $('.hamburger-menu').show();
-  });
+// if (window.matchMedia("(max-width: 1000px)").matches) {
+//   $('.header-right .fas').click(function() {
+//     $('.hamburger-menu').show();
+//   });
+//
+//   $('.hamburger-menu .fas').click(function() {
+//     $('.hamburger-menu').hide();
+//   });
+// }
 
-  $('.hamburger-menu .fas').click(function() {
-    $('.hamburger-menu').hide();
-  });
-} else if (window.matchMedia("(min-width: 999px)").matches) {
-  $('.hamburger-menu').hide();
-
-}
-
+//problema, l'hamburger con display + di 1000px si chiude solo aggiornando
+// serve l'evento resize di jQuery, che è responsive senza dover aggiornare la pagina
 //-----------------------------------------------------------
-
-
-// console.log('ciao');
